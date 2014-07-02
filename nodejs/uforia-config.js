addbc19a04ed41098fd9e39c052df62304bade47
root <root@uforia.dhcp.net>
2014-07-02 20:10:07 +0200

    added a specific config file to separate the config from the code (this time with the actual file added to git)

---
+++ nodejs/uforia-config.js
@@ -0,0 +1,11 @@
+//NodeJs server configuration
+//public variables
+module.exports = {
+	ELASTIC_SEARCH_HOST : "localhost",
+	ELASTIC_SEARCH_PORT : "9200",
+	DATABASE_HOST : "",
+	DATABASE_USER : "",
+	DATABASE_PASSWORD : "",
+	DATABASE_NAME : "uforia",
+	SERVER_PORT : "8888"
+};
