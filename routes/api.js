0500b9f69fe03ba0561df1c3aa010b6cc67fd9e0
Sijmen Vos <sijmenvos@gmail.com>
2014-11-06 20:12:32 +0100

    CORS in api

--- routes/api.js
+++ routes/api.js
@@ -25,6 +25,18 @@ var DEFAULT_VIEW = 'bubble';
 var DEFAULT_VISUALIZATION = 'bar_chart';
 //**********************
 
+router.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Cache-Control");
+  if (req.method === 'OPTIONS') {
+    res.statusCode = 204;
+    return res.end();
+  } else {
+    return next();
+  }
+});
+
 router.post('/get_types', function(req, res) {
   res.send(TYPES);
 });
