d8abc6cd9f4d335ef81809e3dc5a84f53d6f0c07
Sijmen Vos <sijmenvos@gmail.com>
2014-11-06 20:21:11 +0100

    cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('./config.json');
 var debug = require('debug')('Uforia-browser-new');
+var cors = require('cors');
 
 var routes      = require('./routes/index');
 var users       = require('./routes/users'),
@@ -26,6 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', m.init, routes);
 app.use('/api', api);
