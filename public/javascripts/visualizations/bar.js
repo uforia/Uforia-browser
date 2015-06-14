25efde810aea095d638b616de54df8970229172f
Bart Oudhoff <bartoudhoff@gmail.com>
2015-06-14 19:44:51 +0200

    Major changes to the bar chart in order to make it zoomable, still work in progress but useable

--- public/javascripts/visualizations/bar.js
+++ public/javascripts/visualizations/bar.js
@@ -1,47 +1,34 @@
 function render(data, options, openDetails, cb){
-  var margin = {top: 20, right: 20, bottom: 20, left: 40},
-    width = $('#d3_visualization').width(),
-    height = (options.height || window.innerHeight),
-    heightContext = 90,
-    heightFocus = height - heightContext - 20,
-    margin2 = {top: (heightFocus + margin.top + 20), right: 20, bottom: 20, left: 40};
-
-  var dateFormat = d3.time.format("%Y-%m-%dT%H:%M:%S.%LZ");
-  var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S.%LZ").parse;
-
   if(data.values.length == 0){
     cb({error: "No results for this query"});
     return;
   }
 
-  var x, x2;
+  console.log(JSON.stringify(data));
 
-  setScaleType(data.values[0].x);
+  /*  Margin, Width and height */
+  var margin = {top: 20, right: 20, bottom: 30, left: 50, mid: 20};
+  var width = $('#d3_visualization').width() - margin.left - margin.right;
+  var miniHeight = 60;
+  var height = 600 - margin.top - margin.mid - miniHeight - margin.bottom;
 
-  var y = d3.scale.linear()
-      .range([heightFocus, 0]);
-      y2 = d3.scale.linear()
-      .range([heightContext, 0]);
+  var selected;
 
-  var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom"),
-      // .tickFormat(function(d) { return isDate(d) ? dateFormat(new Date(d)) : d; }),
-      xAxis2 = d3.svg.axis()
-      .scale(x2)
-      .orient("bottom")
-      .tickFormat(function(d) { return isDate(d) ? dateFormat(new Date(d)) : d; });
+  var svg = d3.select("#d3_visualization").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.mid + miniHeight + margin.bottom);
 
-  var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left");
-  var yAxis2 = d3.svg.axis()
-      .scale(y2)
-      .orient("left");
+  var barsGroup = svg.append('g')
+            .attr("class","barsGroup")
+            .attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
-  var brush = d3.svg.brush()
-    .x(x2)
-    .on("brush", brushed);
+  var miniGroup = svg.append('g')
+            .attr("class","miniGroup")
+            .attr("transform","translate(" + margin.left + "," + (margin.top + height + margin.mid) + ")");
+
+  var brushGroup = svg.append('g')
+            .attr("class","brushGroup")
+            .attr("transform","translate(" + margin.left + "," + (margin.top + height + margin.mid) + ")");
 
   //Tooltip
   var tip = d3.tip()
@@ -52,130 +39,166 @@ function render(data, options, openDetails, cb){
     "<br /><strong>" + data.y_label + ": </strong>" + d.y;
   });
 
-  var svg = d3.select("#d3_visualization").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-  
-  var focus = svg.append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  barsGroup.call(tip);
 
-  var context = svg.append("g")
-      .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+  /*  Scales */
+  var axisRange = d3.range(data.values.length);
 
-  focus.call(tip);
+  axisRange.shift();
 
-  //Set the max values for the x and y axis
-  x.domain(data.values.map(function(d) { return d.x; }));
-  x2.domain(data.values.map(function(d) { return d.x; }));
-  var max = d3.max(data.values, function(d) { return +d.y; })
-  y.domain([0, max]);
-  y2.domain([0, max]);
-  yAxis.ticks(max);
-  yAxis2.ticks(max);
+  axisRange.push((axisRange[axisRange.length - 1] + 1));
 
-  //add the X axis
-  focus.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + heightFocus + ")")
-      .call(xAxis);
-    // .selectAll("text")
-    //   .attr("transform", "translate(-20, 20) rotate(-45)");
+  var yScale = d3.scale.linear()
+    .range([height, 0])
+    .domain([0, d3.max(data.values, function(d) { 
+      return d.y;
+    })]);
 
-  //Add the Y axis
-  focus.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
+  var yScale2 = d3.scale.linear()
+    .range([miniHeight, 0])
+    .domain([0, d3.max(data.values, function(d) { 
+      return d.y;
+    })]);
+
+  var xScale = d3.scale.ordinal()
+    .rangeBands([0, width], 0.4, 0)
+    .domain(d3.range(data.values.length));
+
+  var xScaleBrush = d3.scale.ordinal()
+    .rangeBands([0, width], 0.4, 0)
+    .domain(d3.range(data.values.length));
+
+  var xScaleAxis = d3.scale.ordinal()
+    .rangeBands([0, width], 0.4, 0)
+    .domain(axisRange);
+
+  /*  Define y axis */
+  var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .tickSize(-width, -width)
+    .ticks(4)
+    .orient("left");
+
+  /* Define y axis */
+  var xAxis = d3.svg.axis()
+    .scale(xScaleAxis)
+    // .tickSize(3,0)
+    // .tickValues([1,10,20,30,40,50,60,70,80,90,100])
+    .orient("bottom");
+
+  /*  Prepare the y axis but do not call .call(xAxis) yet */
+  svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .append("g")
+    .attr("class", "axisLabel")
     .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("dx", "-" + (heightFocus / 2) +  "px")
-      .attr("dy", "-3em")
-      .style("text-anchor", "left")
-      .text(data.y_label);
+    .attr("transform", "translate(" + -(margin.left * 0.8) + "," + (height/2) + "), rotate(-90)")
+    .style("text-anchor", "middle")
+    .text(data.y_label);
 
-  //Add the bars
-  focus.selectAll(".bar")
-      .data(data.values)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.x); })
-      .attr("width", 100)
-      .on("click", mouseclick)
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide)
-      .attr("height", "0px")
-      .attr("y", heightFocus)
-    .transition().duration(500)
-      .attr("y", function(d) { return y(+d.y); })
-      .attr("height", function(d) { return heightFocus - y(+d.y); });
-
-  context.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + heightContext + ")")
-      .call(xAxis);
-
-  //uncomment for a y axis on the bottom 'context' bar chart
-  context.append("g")
-      .attr("class", "y axis")
-      .call(yAxis2)
+  /* Prepare the x axis */
+  svg.append("g")
+    .attr("class","x axis")
+    .attr("transform", "translate(" + margin.left + "," + (margin.top + height + margin.mid + miniHeight) + ")" )
+    .call(xAxis)
+    .append("g")
+    .attr("class", "axisLabel")
     .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("dx", "-" + (heightContext / 2) +  "px")
-      .attr("dy", "-3em")
-      .style("text-anchor", "left")
-      .text(data.y_label);
+    .attr("transform", "translate(" + (width/2) + "," + margin.bottom + ")")
+    .style("text-anchor", "middle") 
+    .text(data.x_label);
 
-  context.selectAll(".contextBar")
-      .data(data.values)
-    .enter().append("rect")
-      .attr("class", "contextBar")
-      .attr("x", function(d) { return x2(d.x); })
-      .attr("width", 10)
-      .attr("height", "0px")
-      .attr("y", heightFocus)
-      .transition().duration(500)
-      .attr("y", function(d) { return y2(+d.y); })
-      .attr("height", function(d) { return heightContext - y2(+d.y); });
+  /* brush */
+  var brush = d3.svg.brush()
+          .x(xScaleBrush)
+          .extent([0, width])
+          .on("brush", display);
 
-  context.append("g")
-      .attr("class", "x brush")
-      .call(brush)
+  brushGroup.append("g")
+    .attr("class", "brush")
+    .call(brush)
     .selectAll("rect")
-      .attr("y", -6)
-      .attr("height", heightContext + 7);
+    .attr("opacity", 0.5)
+    .attr("height", miniHeight);
 
-  function brushed() {
-    x.domain(brush.empty() ? x2.domain() : brush.extent());
-    focus.selectAll(".bar").attr("transform", function(d) { 
-      console.log(x(d.x))
-      return "translate(" + x(d.x) + ",0)"; })
-    focus.select(".x.axis").call(xAxis);
+  function display () {
+    selected =  xScaleBrush.domain()
+                .filter(function(d){
+                  return (brush.extent()[0] <= xScaleBrush(d)) && (xScaleBrush(d) <= brush.extent()[1]);
+                });
+
+    var start;
+    var end;
+
+    /* Keep a minimum amount of bars on there to avoid any jank */
+    if (selected.length > 2 ) {
+      start = selected[0];
+      end = selected[selected.length - 1] + 1;
+    } else {
+      start = 0;
+      end = data.values.length;
+    }
+
+    var updatedData = data.values.slice(start, end);
+
+    // console.log(JSON.stringify(updatedData));
+    updateBars(updatedData);
   }
 
-  //click on a bar
+  function update (grp, data, main) {
+    grp.selectAll("rect").data(data)
+      .attr("x", function (d, i) { return xScale(i); })
+      .attr("width", function (d) { return xScale.rangeBand(); })
+      .attr("y", function (d){ return main ? yScale(d.y) : yScale2(d.y); })
+      .attr("height", function (d) { return main ? height - yScale(d.y) : miniHeight; });
+  }
+
+  function enter (grp, data, main) {
+    grp.selectAll("rect").data(data)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", function (d, i) { return xScale(i);})
+      .attr("width", function(d) { return xScale.rangeBand(); })
+      .attr("y", function (d){ return main ? yScale(d.y) : yScale2(d.y)})
+      .attr("height", function (d) { return main ? height - yScale(d.y) : miniHeight - yScale2(d.y);})
+      .attr("opacity", function () { return 1; })
+      .on("click", mouseclick)
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+  }
+
+  function exit (grp, data) {
+    grp.selectAll("rect").data(data).exit().remove();
+  }
+
+  function updateBars (data) {
+
+    xScale.domain(d3.range(data.length));
+    yScale.domain([0, d3.max(data, function(d) { return d.y;})]);
+
+    /* Update */
+    update(barsGroup, data, true);
+
+    /* Enter… */
+    enter(barsGroup, data, true);
+
+    /* Exit */
+    exit(barsGroup, data);
+
+    /* Call the Y axis to adjust it to the new scale */
+    svg.select(".y")
+      .transition()
+      .duration(10)
+      .call(yAxis);
+  }
+
   function mouseclick(d){
     openDetails({hashids: d.hashids, adressses:[d.x], tables: d.tables});
   }
 
-  function isDate(val) {
-    var d = new Date(val);
-    return !isNaN(d.valueOf());
-  }
-
-  //Determines wether the x axis is a date or some other value and set the x domain accordingly
-  function setScaleType(val){
-    if(isDate(val)){
-        x = d3.time.scale().range([0, width]);
-        x2 = d3.time.scale().range([0, width]);
-    }  else {
-        x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-        x2 = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-    }
-
-
-    //Default to ordinal for testing -- REMOVE THIS LATER
-    x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-    x2 = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-  }
+  enter(miniGroup, data.values, false);
+  updateBars(data.values);
   cb();
 }
-
