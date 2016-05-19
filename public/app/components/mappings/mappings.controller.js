aa4c6b14bc7ceea8afb1e76e562a5b6c0ebaeca6
Moduo <tom-oud@hotmail.com>
2016-05-19 14:38:24 +0200

    Edited styling from user and mapping views. Also created placeholder for status in mappings if the mapping is ready.

--- public/app/components/mappings/mappings.controller.js
+++ public/app/components/mappings/mappings.controller.js
@@ -1,5 +1,5 @@
-(function() {
-    var mod = angular.module('mappings',['mappings.create', 'mappings.edit', 'mappings.visualizations']);
+(function () {
+    var mod = angular.module('mappings', ['mappings.create', 'mappings.edit', 'mappings.visualizations']);
 
     mod.controller('MappingsController', MappingsController);
 
@@ -9,40 +9,29 @@
         $scope.test = [];
         $scope.fields = ['test', 'blaa'];
         $scope.deleteMapping = deleteMapping;
-        // $scope.deleteMapping = function(type, index){
-        //   if(confirm("Are you sure you want to delete the mapping '" + type + "'?")){
-        //     $http.post('./api/delete_mapping', {type: type})
-        //         .success(function(data){
-        //           $scope.types.splice(index, 1);
-        //         })
-        //   }
-        // }
 
-        $scope.pauseFilling = function(type){
-          socket.emit('pauseFilling', {type: type});
+        $scope.pauseFilling = function (type) {
+            socket.emit('pauseFilling', { type: type });
         }
 
-        console.log($rootScope);
-
-
         function deleteMapping(mapping) {
-            console.log("test");
+
             var modalInstance = $modal.open({
                 templateUrl: 'app/components/mappings/modals/deleteMappingModal.html',
                 controller: 'deleteMappingModalController',
                 size: 'md',
                 resolve: {
-                    mapping: function() {
+                    mapping: function () {
                         return mapping;
                     }
                 },
             });
 
-            modalInstance.result.then(function() {
-                // MappingsController();
-            },
-            function() {
-            });
+            modalInstance.result.then(function () {
+
+                },
+                function () {
+                });
         }
     }
 })();
