0e8f40149a19c5dd056ce0e4d38c8dfbbb40a75f
charlie5991 <charlotte@hanekamp.be>
2016-05-26 14:32:30 +0200

    delete comments

--- public/app/components/cases/create/create.controller.js
+++ public/app/components/cases/create/create.controller.js
@@ -27,36 +27,12 @@
             toastr.error('Please try again later.', 'Something went wrong!');
         });
 
-        // $http({
-        //     method: 'GET',
-        //     url: '/api/get_cases'
-        // }).then(function successCallback(data) {
-        //     // Check if error
-        //     if (typeof data.data.error !== 'undefined') {
-        //         $scope.error.push(data.data.error.message);
-        //         $scope.isError = true;
-        //     } else if (data.data.response.hits.total > 0) {
-        //
-        //         users = data.data.response.hits.hits;
-        //     }
-        // }, function errorCallback(data) {
-        //     toastr.error('Please try again later.', 'Something went wrong!');
-        // });
-        //
-        // angular.forEach(users, function(value, key){
-        //     var u = value._source;
-        //     // if(user.email == u.email){
-        //         // addCase = false;
-        //     // }
-        // });
 
         $scope.save = function(cases){
             var addCase = true;
-            console.log(cases);
-            // Save user
+            // Save case
             if(addCase) {
                 var tempCase = angular.copy(cases);
-                // delete tempCase.password2;
                 $http.post('/api/save_case', tempCase)
                     .success(function (data) {
                             if (typeof data.error !== 'undefined') {
